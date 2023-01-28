24d1d9447a0f39eb537f8f724247fd0a2256e9dc
Will Roberts <wroberts0125@gmail.com>
2023-01-28 13:35:39 -0500

    Initial Commit

---
+++ YouAreAwesome-Roberts/YouAreAwesome_RobertsApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwesome_RobertsApp.swift
+//  YouAreAwesome-Roberts
+//
+//  Created by Will Roberts on 1/28/23.
+//
+
+import SwiftUI
+
+@main
+struct YouAreAwesome_RobertsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
