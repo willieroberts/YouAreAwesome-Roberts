5d097e770c97f53ffb380ef790abf61ff7241ce4
Will Roberts <wroberts0125@gmail.com>
2023-01-28 13:43:50 -0500

    HW1 final

--- YouAreAwesome-Roberts/ContentView.swift
+++ YouAreAwesome-Roberts/ContentView.swift
@@ -1,21 +1,39 @@
 //
 //  ContentView.swift
-//  YouAreAwesome-Roberts
+//  YouAreAwesome
 //
-//  Created by Will Roberts on 1/28/23.
+//  Created by Will Roberts on 1/26/23.
 //
 
 import SwiftUI
 
 struct ContentView: View {
+    @State private var messageString = ""
+    
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text(messageString)
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .foregroundColor(.red)
+                .padding()
+            
+            HStack {
+                
+                Button("Awesome") {
+                    messageString = "You Are Awesome!"
+                    
+                }
+                .buttonStyle(.borderedProminent)
+                
+                Button("Great") {
+                    messageString = "You Are Great!"
+                    
+                }
+                .buttonStyle(.borderedProminent)
+            }
         }
-        .padding()
     }
 }
 
